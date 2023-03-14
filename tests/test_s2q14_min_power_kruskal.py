ee7a5da4a374e6f99f7a036f3e3e042700060e45
souldxaklouche <sarah.ouldaklouche@ensae.fr>
2023-03-14 22:11:08 +0000

    code commenté

---
+++ tests/test_s2q14_min_power_kruskal.py
@@ -0,0 +1,23 @@
+    """
+    Test de la fonction min_power
+    """
+
+# This will work if ran from the root folder.
+from os import chdir
+chdir("/home/onyxia/work/ensae-prog23/delivery_network")
+
+from graph import graph_from_file
+import unittest   # The test framework
+
+class Test_MinimalPower(unittest.TestCase):
+    def test_network0(self):
+        g = graph_from_file("/home/onyxia/work/ensae-prog23/input/network.00.in")
+        self.assertEqual(g.min_power_kruskal(1, 4)[0], 11)
+        self.assertEqual(g.min_power_kruskal(2, 4)[0], 10)
+
+    def test_network1(self):
+        g = graph_from_file("/home/onyxia/work/ensae-prog23/input/network.04.in")
+        self.assertEqual(g.min_power_kruskal(1, 4)[0], 4)
+
+if __name__ == '__main__':
+    unittest.main()
